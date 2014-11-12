edd1c61e00314774771ce3a11178e5cf61e58177
Montana <montanawong@gmail.com>
2014-11-12 08:57:44 -0500

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,7 +1,8 @@
-
 public class Hand extends Deck {
 	private int size = 2;
-	private Card[] hand = new Card[10];
+	private Card[] hand = new Card[11]; 
+	// the absolute largest hand one can have in blackjack is 11
+	// that is 4 "aces", 4 "two's", and 3 "threes"
 	
 	public Hand(){
 		this.hand[0] = null;
@@ -27,15 +28,19 @@ public class Hand extends Deck {
 	
 	public void printContents(){
 		for (int x = 0; x < this.size; x++){
-			System.out.println(this.hand[x].toString());
+			System.out.print(this.hand[x].toString() + " ");
 		}
+		System.out.println("\n");
+	}
+	public void clearHand() {
+		this.hand = new Card[11];
+		this.size = 2;
 	}
 	public boolean isBlackJack(){
 		return ((this.hand[0].getRank().equals("Ace") 
 			&& (this.hand[1].getValue() ==10))
 			|| ((this.hand[1].getRank().equals("Ace"))
 			&& (this.hand[0].getValue() == 10)) 
-			&& (size == 2));
+			&& (this.size == 2));
 	}
-
 }
